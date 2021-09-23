6dea8298d4911339e5759fc0795ae46378387b65
ljor <leah.m.jorgensen@gmail.com>
2021-09-22 23:36:26 -0400

    Server set up

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+// Database Setup
+const mongoURI = process.env.MONGODB_URI
+const db = mongoose.connection
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log('Database connected')
+})
+
+db.on('error', (err) => {console.log('Error:', err)})
+db.on('connected', () => {console.log('Mongo connected')})
+db.on('disconnected', () => {console.log('Mongo disconnected')})
+
+// Static Assets
+app.use(express.static('public'))
+
+// Middleware
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+// Controllers
+
+// Listener
+app.listen(PORT, () => {
+    console.log('Listening on port: ', PORT)
+})
