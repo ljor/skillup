3449b17116b9bd6fce863f7bbba07fef62dd875a
ljor <leah.m.jorgensen@gmail.com>
2021-09-25 13:58:10 -0400

    Challenge seed data added

---
+++ models/webDevSeed.js
@@ -0,0 +1,128 @@
+module.exports = [
+    {
+        name: 'Third party API',
+        description: 'Create a site that consumes a third party API',
+        challenge: 'Almost every company you work for, is going to consume an API, whether it\'s a web or library API, they are going to consume an API from somewhere and you can integrate it. This is a great skill set to have. You do not need to build a complex backend since the API you are using takes care of that for you.',
+        skill: 'web development'
+    },
+    {
+        name: 'Brochure Site',
+        description: 'Create a small brochure website',
+        challenge: 'This can be a landing page, artist page. Usually it\'s a site that showcases a business or a contact page. You can build this for an existing business. This site is to show an appealing, high converting landing page. The key is to make it look visually pleasing. You can work for companies that need a landing page. This is a great secondary skill that one can possess.',
+        skill: 'web development'
+    },
+    {
+        name: 'E-Commerce Solutions',
+        description: 'Develop the back-end of an e-commerce website',
+        challenge: 'Create the back-end of an e-commerce website. Set up payments, the library of products that need to be sold, inventory and more. All you need is the basic interface of a shop on the front end. For an extra challenge you can add user authentication, make a really complex inventory system, or add a shopping cart system.',
+        skill: 'web development'
+    },
+    {
+        name: 'Personal Portfolio',
+        description: 'Build your own portfolio website',
+        challenge: 'Create a website where you can showcase all your projects. While building this, you can choose to showcase your creative side as well as keep things professional in case you want to share it with a potential client. Make sure you spend a lot of time on the front end of your portfolio and make it look appealing.',
+        skill: 'web development'
+    },
+    {
+        name: 'Matching Game',
+        description: 'Create a matching game',
+        challenge: 'Create a game where the players try to make matches of cards or tiles of your choosing. The objective is to find all the matches. Add a way to let users know that they’ve won the game. For an extra challenge, you can add a scoring or timing system where players can try to beat their best score -- or the score of other players.',
+        skill: 'web development'
+    },
+    {
+        name: 'To-Do List',
+        description: 'Create a to-do list app',
+        challenge: 'Build a web app that allows you to make to-do lists for routine tasks. Allow users to add, check-off, edit, and delete content on their list. Include the date and options to save the list.',
+        skill: 'web development'
+    },
+    {
+        name: 'Address Book',
+        description: 'Create an address book app',
+        challenge: 'Build an application that can search for particular entries in the address book by filtering the attributes you specify. You can either use an API that generates placeholder data, or you can also structure the JSON (JavaScript Object Notation). Once the data is in place, you must load it in your application by using an AJAX request (jQuery or XML HTTP request) just as you would in a real-world application. Also, you can design the web application to cache requests in the local storage to avoid unnecessary network requests.',
+        skill: 'web development'
+    },
+    {
+        name: 'Form Validation',
+        description: 'Create a simple form validation',
+        challenge: 'Create a form that takes a name, surname, and password and validates the information.  Add character limits to it. If it exceeds, you devise an error that shows up. For the password, require a mixture of special characters, numbers, and ensure the password is accepted once this is done.',
+        skill: 'web development'
+    },
+    {
+        name: 'Drawing App',
+        description: 'Create a drawing app',
+        challenge: 'Create an app that allows users to draw on a canvas and save their work.. A mouse can be used to draw. Other key features in this include: colour change, size of drawing tool, reset canvas and erase. As an additional feature, you can let users share their work on social media platforms as well.',
+        skill: 'web development'
+    },
+    {
+        name: 'Countdown Timer',
+        description: 'Create an event countdown timer app',
+        challenge: 'This is a virtual timer that counts a certain date to tell you the beginning or the end of a particular event. Essentially, it\'s the web page that updates the time every second. The goal here is to display day, hours, minutes and seconds. JS can be used for this function. A basic countdown timer can have a pause, start, stop button. An alert notification is helpful too once the event time has been achieved.',
+        skill: 'web development'
+    },
+    {
+        name: 'Word Counter',
+        description: 'Create a word counter app',
+        challenge: 'Develop an app that counts the number of words in an article. This is a helpful tool for copywriters when they have to keep watch at the word limit for a particular article. It requires you to build an app that parses texts and highlights the number of words. Once the user enters all their material, the number of words should be displayed. Additionally, you can have the app display the number of characters, paragraphs, sentences and keywords in the user’s article.',
+        skill: 'web development'
+    },
+    {
+        name: 'Exit Plugin',
+        description: 'Create an site exit plugin or widget',
+        challenge: 'You must have seen this while browsing through a website, a pop-up that shows up before you are about to leave the website. Companies usually use exit plugins to show exciting offers to keep a user on the page. This is precisely what you have to design. You can create this exit plugin to go with a project you already have, or design it as a stand-alone to learn how to create it.',
+        skill: 'web development'
+    },
+    {
+        name: 'Chat Application',
+        description: 'Create a real-time chat app',
+        challenge: 'Develop a real-time chat app, where users can communicate with each other. This can be a stand-alone app or one that is incorporated into another project (think collaborative focused sites). Include a feature where an update is made and it auto-updates across all users in real-time.',
+        skill: 'web development'
+    },
+    {
+        name: 'Permissions and Roles',
+        description: 'Add permissions and roles to a website',
+        challenge: 'The good thing about working on this is that you can add this to any existing project. What you really want to do with this is focus on user permissions. Every website has their own set of rules when it comes to permissions for users. Some can use, some cannot, some have access to a certain kind of information while some have limited access. It might be hard to manage these permissions in a clean way. Working on this early in your career will equip you with the right skill set and knowledge',
+        skill: 'web development'
+    },
+    {
+        name: 'Charting Dashboards',
+        description: 'Create a dashboard that uses charts',
+        challenge: 'Create a charting dashboard. This can be an admin page that gives you details about logged in users, purchases, essentially, visual data that brings data to life. Most of the time when you work at a company, businesses will want visual charts to represent data as they are non-technical and easy to understand. You can use any charting library to perform this function. There are several APIs out there to find a data source.',
+        skill: 'web development'
+    },
+    {
+        name: 'Ratings and Reviews',
+        description: 'Create a rating and review system',
+        challenge: 'Develop a rating and review system where you can have ratings and anonymous reviews. This will require algorithmic work on which reviews appear at the top and get shown to audiences first. Include a feature on weeding out fake comments and keeping only the genuine reviews on your site.',
+        skill: 'web development'
+    },
+    {
+        name: 'Restaurant Website',
+        description: 'Create a website for a restaurant',
+        challenge: 'Create a stunning web page for a restaurant. The task will be to align the various food items and beverages using a CSS layout grid. Along with this, adding pricing features, photos of the food items to beautify it, a combination of colors, fonts, etc. can be added. Internal linking of pages is also advised to gain better traction.',
+        skill: 'web development'
+    },
+    {
+        name: 'Tribute Page',
+        description: 'Create a tribute webpage',
+        challenge: 'Create a tribute webpage, add an image of the person you are giving a tribute and create a small write up. Paragraphs, links, lists, photos with CSS can be added to this. Make sure the font is uniform and add a background colour to your web page.',
+        skill: 'web development'
+    },
+    {
+        name: 'Survey Form',
+        description: 'Create a survey form',
+        challenge: 'In this project, you will have to design a full-fledged survey form that includes relevant questions like name, age, email, address, contact number, and other questions, depending on the type of company or organization you are shaping the form. This project will put to the test your webpage structuring skills.',
+        skill: 'web development'
+    },
+    {
+        name: 'AJAX-Style Login',
+        description: 'Create an AJAX-style login',
+        challenge: 'The focus of this project is to build the front-end of an AJAX-style login site/page. In AJAX-style login, the login page does not need to be reloaded to ensure whether or not you have to input the correct login details.',
+        skill: 'web development'
+    },
+    {
+        name: 'Toast Notification',
+        description: 'Create a toast notification',
+        challenge: 'A toast notification is an unobtrusive and non-modal window element that is used to display brief, auto-expiring information to users. You can see toast notifications primarily on Android OS platforms. For this project, you will design a functional toast notification tool that can respond to events on the page and notify the users as and when an event has completed successfully. You could also use a time delay feature to represent the delay in loading or saving data.',
+        skill: 'web development'
+    }
+]
